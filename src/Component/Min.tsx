06de15e18b10648e2bce8fd8669b1579927a8f99
amirsachmeh <amirhs.dma@gmail.com>
2024-03-20 20:36:36 +0330

    first-gam

---
+++ src/Component/Min.tsx
@@ -0,0 +1,26 @@
+
+
+const Main : React.FC = ()=> {
+    return (
+    <main>
+        <section className='start-app'>
+            <h3 className="date">june7/10AM/townAhal</h3>
+            <h1 className="Main-title">Finde your Hous</h1>
+            <div className="start-box">
+                <div className="name-title-box">
+                    <h3 className="Company-name">Losantosina and brothers</h3>
+                    <h4 className="sec-title">the bigest realEstate in area</h4>
+                </div>
+                <button className="start-btn">Start Now</button>
+            </div>
+        </section>
+        <section className='About'></section>
+        <section className='User-code'></section>
+        <section className='Reviews'></section>
+        <footer className='Contact'></footer>
+    </main>
+    )
+  }
+  
+  export default Main;
+  
