8db01f026d544a4168669d40da110978cae65363
amirsachmeh <amirhs.dma@gmail.com>
2024-03-21 20:02:18 +0330

    secend-part

--- src/Component/Min.tsx
+++ src/Component/Min.tsx
@@ -1,20 +1,32 @@
-
+import StartApp from "./StartApp";
+import AXha from '../Imagez/hose.jpg'
 
 const Main : React.FC = ()=> {
     return (
     <main>
-        <section className='start-app'>
-            <h3 className="date">june7/10AM/townAhal</h3>
-            <h1 className="Main-title">Finde your Hous</h1>
-            <div className="start-box">
-                <div className="name-title-box">
-                    <h3 className="Company-name">Losantosina and brothers</h3>
-                    <h4 className="sec-title">the bigest realEstate in area</h4>
-                </div>
-                <button className="start-btn">Start Now</button>
-            </div>
+        <StartApp/>
+        <section className='About'>
+            <h1 className="About-title"> Losantos & Brothers Realty in Vancouver</h1>
+            <p className="About-description">In the heart of Vancouver, 
+                Losantos & Brothers Realty stands 
+                out as a premier destination for 
+                exquisite office and residential 
+                buildings 
+                for <a className="About-link" href="#">
+                <b>
+                sale and rent.
+                </b>
+                </a>
+                With a perfect blend of elegance
+                and functionality, our properties
+                redefine luxury living in Vancouver
+                Experience unparalleled quality 
+                and service as you explore the 
+                exceptional offerings by 
+                Losantos & Brothers Realty.
+                </p>
+            <img src={AXha} alt="ax"  className="image"/>
         </section>
-        <section className='About'></section>
         <section className='User-code'></section>
         <section className='Reviews'></section>
         <footer className='Contact'></footer>
